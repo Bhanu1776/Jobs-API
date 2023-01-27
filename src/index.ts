673e09e7cb07bbcef7efafad3af1f3a605922db3
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-01-27 18:28:32 +0530

    :sparkles: (Express) Created server with Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello world");
+import { Express, Request, Response } from "express";
+const express = require("express");
+const app: Express = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req: Request, res: Response) => {
+  //   res.send("Hi, I am Live Now");
+  res.json({ msg: "I am root" });
+});
+
+const start = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Yes I am connected to ${PORT} Port`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
