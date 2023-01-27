492701c71262c220bb2ec225c8c5101263951b7d
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-01-28 00:26:45 +0530

    :recycle: (App) Moved Files to src folder

---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,8 @@
+"use strict";
+const express = require("express");
+const router = express.Router();
+const { getAllJobs, getAllJobsTesting } = require("../controllers/Jobs");
+router.route("/").get(getAllJobs);
+router.route("/testing").get(getAllJobsTesting);
+module.exports = router;
+//# sourceMappingURL=AppRoutes.js.map
