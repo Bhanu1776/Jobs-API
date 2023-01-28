c22f7bc066f4278c2492938162772b63f320ec31
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-01-28 13:38:56 +0530

    :card_file_box: (DB) Added Jobs Schema

---
+++ src/models/JobSchema.ts
@@ -0,0 +1,29 @@
+const mongoosee = require("mongoose");
+
+const jobSchema = new mongoosee.Schema({
+  title: {
+    type: String,
+    required: [true, "title must be provided"],
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+  description: {
+    type: String,
+    required: [true, "description must be provided"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Price must be provided"],
+  },
+  category: {
+    type: String,
+    required: false,
+  },
+  image: {
+    type: String,
+  },
+});
+
+module.exports = mongoosee.model("Jobs", jobSchema);
