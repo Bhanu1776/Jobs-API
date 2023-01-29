ac9e5030d8a11b05453191ada97ea9eb5e7d8c05
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-01-29 14:32:32 +0530

    :sparkles: (App) Added Pagination in API

--- build/controllers/Jobs.js
+++ build/controllers/Jobs.js
@@ -12,33 +12,47 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getAllJobsTesting = exports.Temp = exports.getAllJobs = void 0;
 const JobSchema = require("../models/JobSchema");
 const getAllJobs = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { category, title, sort } = req.query;
-    const queryObject = {
-        title: undefined,
-        description: undefined,
-        price: undefined,
-        category: undefined,
-    };
-    if (category) {
-        queryObject.category = category;
-    }
-    if (title) {
-        queryObject.title = { $regex: title, $options: "i" };
-    }
-    // let myArray:any = sort;
+    const { category, title, sort, select } = req.query;
+    // type Obj = {
+    //   title: any;
+    //   description: any;
+    //   price: any;
+    //   category: any;
+    // };
+    // var queryObject: Obj = {
+    //   title: undefined,
+    //   description: undefined,
+    //   price: undefined,
+    //   category: undefined,
+    // };
+    // if (category) {
+    //   queryObject.category = category;
+    // }
+    // if (title) {
+    //   queryObject.title = { $regex: title, $options: "i" };
+    // }
+    // let myArray: any = sort;
     // let splitArray = [];
     // for (let i = 0; i < myArray.length; i++) {
-    //   splitArray.push(myArray[i].split(" "));
+    //   splitArray.push(myArray[i].split(",").join(" "));
     // }
     // console.log(splitArray);
-    let apiData = JobSchema.find(queryObject);
-    if (sort) {
-        let sortSafe = sort;
-        let sortFix = sortSafe.split(",").join(" ");
-        apiData = apiData.sort(sortFix);
-    }
+    let apiData = JobSchema.find(req.query);
+    // if (sort) {
+    //   let sortFix = splitArray.split(",").join(" ");
+    //   apiData = apiData.sort(sortFix);
+    // }
+    // if (select) {
+    //   // let selectFix = select.replace(",", " ");
+    //   let selectFix = select.split(",").join(" ");
+    //   apiData = apiData.select(selectFix);
+    // }
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 10;
+    let skip = (page - 1) * limit;
+    apiData = apiData.skip(skip).limit(limit);
     const myData = yield apiData;
-    res.status(200).json({ myData });
+    res.status(200).json({ myData, nbHits: myData.length });
 });
 exports.getAllJobs = getAllJobs;
 const Temp = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
--- src/controllers/Jobs.ts
+++ src/controllers/Jobs.ts
@@ -2,47 +2,59 @@ import { Request, Response } from "express";
 const JobSchema = require("../models/JobSchema");
 
 export const getAllJobs = async (req: Request, res: Response) => {
-  const { category, title, sort } = req.query;
+  const { category, title, sort, select } = req.query;
 
-  type Obj = {
-    title: any;
-    description: any;
-    price: any;
-    category: any;
-  };
+  // type Obj = {
+  //   title: any;
+  //   description: any;
+  //   price: any;
+  //   category: any;
+  // };
 
-  const queryObject: Obj = {
-    title: undefined,
-    description: undefined,
-    price: undefined,
-    category: undefined,
-  };
+  // var queryObject: Obj = {
+  //   title: undefined,
+  //   description: undefined,
+  //   price: undefined,
+  //   category: undefined,
+  // };
 
-  if (category) {
-    queryObject.category = category;
-  }
-  if (title) {
-    queryObject.title = { $regex: title, $options: "i" };
-  }
+  // if (category) {
+  //   queryObject.category = category;
+  // }
+  // if (title) {
+  //   queryObject.title = { $regex: title, $options: "i" };
+  // }
 
-  // let myArray:any = sort;
+  // let myArray: any = sort;
   // let splitArray = [];
 
   // for (let i = 0; i < myArray.length; i++) {
-  //   splitArray.push(myArray[i].split(" "));
+  //   splitArray.push(myArray[i].split(",").join(" "));
   // }
   // console.log(splitArray);
 
-  let apiData = JobSchema.find(queryObject);
+  let apiData = JobSchema.find(req.query);
 
-  if (sort) {
-    let sortSafe: any = sort;
-    let sortFix = sortSafe.split(",").join(" ");
-    apiData = apiData.sort(sortFix);
-  }
+  // if (sort) {
+  //   let sortFix = splitArray.split(",").join(" ");
+  //   apiData = apiData.sort(sortFix);
+  // }
+
+  // if (select) {
+  //   // let selectFix = select.replace(",", " ");
+  //   let selectFix = select.split(",").join(" ");
+  //   apiData = apiData.select(selectFix);
+  // }
+
+  let page = Number(req.query.page) || 1;
+  let limit = Number(req.query.limit) || 10;
+
+  let skip = (page - 1) * limit;
+
+  apiData = apiData.skip(skip).limit(limit);
 
   const myData = await apiData;
-  res.status(200).json({ myData });
+  res.status(200).json({ myData, nbHits: myData.length });
 };
 
 export const Temp = async (req: Request, res: Response) => {
